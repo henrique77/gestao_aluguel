1286251106f9d9e516934174fed102448d1b1db5
henrique77 <henriquecaires@outlook.com>
2023-03-06 23:54:46 -0300

    Ajuste nas configurações do docker

--- gestao_aluguel/settings.py
+++ gestao_aluguel/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'gestao',
         'USER': 'root',
         'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
